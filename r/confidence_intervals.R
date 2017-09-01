a8f69067e3b5d98921ead3f80b2056e8613ca1be
AndreasChristianHill <andreas.hill@usys.ethz.ch>
2017-09-01 10:07:07 +0200

    update Andreas 01.09.2017(1)

--- r/confidence_intervals.R
+++ r/confidence_intervals.R
@@ -18,18 +18,20 @@ library(forestinventory)
 ## load grisons dataset:
 data(grisons)
 
-## 1) calculate onephase-estimation for entire dataset:
+## calculate onephase-estimation for entire dataset:
 op <- onephase(formula = tvol~1 ,data = grisons,
                phase_id =list(phase.col = "phase_id_2p",terrgrid.id = 2))
-summary(op)
+
+## Confidence Intervals:
 confint(op)
 
-## 2) calculate onephase-estimation for given domains (areas) in dataset:
+## 2) calculate one-phase-estimation for given domains (areas) in dataset:
 op.a <- onephase(formula = tvol~1,
                  data = grisons,
                  phase_id = list(phase.col = "phase_id_2p", terrgrid.id = 2),
                  area = list(sa.col = "smallarea", areas = c("A", "B")))
-summary(op.a)
+
+## Confidence Intervals:
 confint(op.a)
 
 
@@ -37,56 +39,22 @@ confint(op.a)
 # ----- CI for Two-Phase Estimation --------------- #
 #####################################################
 
-
+## calculate two-phase-estimation for entire dataset:
 extpsynth_2p<- twophase(formula = tvol ~ mean + stddev + max + q75, data = grisons,
                         phase_id = list(phase.col = "phase_id_2p", terrgrid.id = 2),
                         small_area = list(sa.col = "smallarea", areas = c("A", "B","C", "D"),
                                           unbiased = TRUE))
-
+## Confidence Intervals:
 confint(extpsynth_2p)
 
-## CIs with bonferroni correction
+## change confidence level:
+confint(extpsynth_2p, level = 0.9)
+
+## CIs with bonferroni correction to better meet overall coverage probability
+#  (multiple testing problem if a large number of CIs are compared to each other)
 confint(extpsynth_2p, adjust.method = "bonferroni")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 #######
 # END #
 #######
-
-
